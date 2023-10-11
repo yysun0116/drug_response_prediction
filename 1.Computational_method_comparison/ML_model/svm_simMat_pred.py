d85cba8aa5580cff6699dea0b132dff7b7f83668
yihyun <jessie.yy.sun@gmail.com>
2023-10-11 10:59:11 +0800

    update import fileName

--- 1.Computational_method_comparison/ML_model/svm_simMat_pred.py
+++ 1.Computational_method_comparison/ML_model/svm_simMat_pred.py
@@ -9,7 +9,7 @@ import os
 
 datadir = '/Users/yihyun/Code'
 train_set = pd.read_csv(os.path.join(datadir, "drug_sensitivity_prediction/0.data/processed/prism_train.csv"), index_col="smiles")
-test_set = pd.read_csv(os.path.join(datadir, "drug_sensitivity_prediction/0.data/processed/prism_test.csv"), index_col="smiles")
+test_set = pd.read_csv(os.path.join(datadir, "drug_sensitivity_prediction/0.data/processed/prism_drugBlind_test.csv"), index_col="smiles")
 cell_line_ls = train_set.columns.tolist()
 cell_line_ls = train_set.columns.tolist()
 train_mol_all = train_set.index.tolist()
